0c1298f8ba989f4f48f6216580f16d383ebef39b
Mohit Rai <mohiit.rai.007@gmail.com>
2025-05-03 00:25:42 +0530

    Project: Band_Name_Generator

---
+++ Day-01/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator \n Created by Mohit Rai")  
+city_name=input("What's the name of the city you grew up in?\n")  
+pet_name=input("What's Your Pet Name?\n")  
+print("Your Band name could be "+ city_name +" "+ pet_name)
